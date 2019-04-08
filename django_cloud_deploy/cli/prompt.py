ef83cf6656694e4ec06fd40b83a18bc76946b759
Enrique Padilla <enriquejosepadilla@gmail.com>
2019-04-08 03:24:47 -0700

    Only allow open billing accounts

--- django_cloud_deploy/cli/prompt.py
+++ django_cloud_deploy/cli/prompt.py
@@ -828,9 +828,10 @@ class BillingPrompt(TemplatePrompt):
         billing_accounts = self.billing_client.list_billing_accounts()
         billing_account_names = [
             account['name'] for account in billing_accounts
+            if account.get('open')
         ]
         if s not in billing_account_names:
-            raise ValueError('The provided billing account does not exist.')
+            raise ValueError('The provided billing account does not exist or is not eligible to use.')
 
 
 class PostgresPasswordPrompt(TemplatePrompt):
